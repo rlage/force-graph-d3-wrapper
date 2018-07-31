6653309cdac07a8c5fedc2fbd2a7d560c9eba496
Rafael Lage <rafael.lage@xing.com>
2018-07-31 20:43:40 +0200

    add first options for svg element: width and height

--- src/js/force-graph-d3-wrapper.js
+++ src/js/force-graph-d3-wrapper.js
@@ -1,3 +1,13 @@
-function forceGraphD3Wrapper() {
-  return {}
+const defaultOptions = {
+  svg : {
+    width: '100%',
+    height: '100%',
+  }
+}
+function forceGraphD3Wrapper(classElement, data, options = defaultOptions) {
+  const svgOptions = options.svg
+  let svg = d3.select(`.${classElement}`).append('svg')
+  svg.attr('height', svgOptions.height)
+  svg.attr('width', svgOptions.width)
+  return this
 }
